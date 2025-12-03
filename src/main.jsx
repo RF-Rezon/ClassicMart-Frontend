581906e5b6fbed0da8c1e852a97b281da20a6925
RF-Rezon <rezwanfaysalrezon@gmail.com>
2025-12-03 21:48:34 +0600

    vercel speed pack installed

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
@@ -6,12 +7,13 @@ import WaitHandler from "./Handler/WaitHandler.jsx";
 import "./index.css";
 import { router } from "./Routes/Router.jsx";
 
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
       <WaitHandler>
-        <RouterProvider router={router} />
+        <RouterProvider router={router} >
+          <SpeedInsights />
+        </RouterProvider>
       </WaitHandler>
     </AuthProvider>
   </React.StrictMode>
